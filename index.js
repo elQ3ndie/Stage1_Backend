220c08ed24f170e4d487dad0ed3435696887e42e
elcarim <andrewmiracle300@gmail.com>
2023-09-10 22:43:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+    const {slack_name, track} = req.query;
+
+    if (!slack_name || !track) {
+        return res.status(400).json({ error: 'slack_name and track are required query parameters' });
+    }
+
+    const currentDate = new Date()
+
+    const current = currentDate.getDay();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = daysOfWeek[current];
+
+    const currentUTC = currentDate.toISOString();
+
+    const githubFileURL = "";
+    const githubRepoURL = ""
+
+    const jsonResponse = {
+        slack_name,
+        current_day: currentDay,
+        utc_time: currentUTC,
+        track,
+        github_file_url: githubFileURL,
+        github_repo_url: githubRepoURL,
+        status_code: 200,
+      };
+    
+      res.json(jsonResponse);
+    
+});
+
+
+app.listen(port,() => {{}
+console.log("Port is running on http://localhost:${port}");
+});
+
+
